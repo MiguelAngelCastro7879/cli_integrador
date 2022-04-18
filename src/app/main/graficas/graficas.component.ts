0f8d96f8b2b0539bdb6bfac975d41458d9bc97a0
PedroDSM <pedro.koreano219@gmail.com>
2022-04-18 10:22:00 -0500

    cambio menu

--- src/app/main/graficas/graficas.component.ts
+++ src/app/main/graficas/graficas.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-graficas',
   templateUrl: './graficas.component.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraficasComponent implements OnInit {
 
+  @ViewChild("newCanvas ")
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    new Chart(elemento, configs);
   }
 
 }
